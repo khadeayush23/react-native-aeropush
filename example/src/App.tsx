5cc3469a2419f8a0f5e179167fe9f76c542c8283
Ayush Khade <khadeayush23@gmail.com>
2026-04-10 17:38:47 +0530

    initial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ const result = multiply(3, 7);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result: 40</Text>
     </View>
   );
 }
